ec92c24d1657bd8a54dfea876bff06c2643e02e5
Vlad Harkusha <sivari888@gmail.com>
2025-01-31 20:40:02 +0200

    header user info was added

--- src/components/auth-user-header-info/AuthUserHeaderInfo.tsx
+++ src/components/auth-user-header-info/AuthUserHeaderInfo.tsx
@@ -1,15 +1,18 @@
 import {FC} from "react";
 import './AuthUserHeaderInfo.scss';
+import {useAppSelector} from "../../redux/store.ts";
 
 export const AuthUserHeaderInfo:FC = () => {
+    const { authUser } = useAppSelector(store => store.authStoreSlice);
+
     return (
         <div className='auth-user-info'>
             <div>
-                <p className='user-firstname'>UserName</p>
-                <p className='user-lastname'>UserLastName</p>
+                <p className='username'>username: {authUser?.username}</p>
+                <p className='user-email'>{authUser?.email}</p>
             </div>
             <div className='user-photo-wrapper'>
-                <img src="https://dummyjson.com/icon/elijahs/128" alt="user-photo"/>
+                <img src={authUser?.image} alt="user-photo"/>
             </div>
         </div>
     );
