012c120fced90648add1a910b9dfa747b54002d3
Vlad Harkusha <sivari888@gmail.com>
2025-02-01 14:36:46 +0200

    add some styles

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -1,12 +1,25 @@
 import {FC} from "react";
 import './HomePage.scss';
 import {Loader} from "../../components/UI/loader/Loader.tsx";
+import {Link} from "react-router-dom";
+import {urlEndpoints} from "../../router/constans/urlEndpoints.ts";
+import {useAppSelector} from "../../redux/store.ts";
 
 export const HomePage:FC = () => {
+    const { authUser } = useAppSelector(state => state.authStoreSlice);
+
     return (
         <div className='home-page'>
-            <h2>Home page</h2>
+            <h2 className='home-page-title'>Home page</h2>
+            <p className='home-page-text'>This test application is created to practice React, TypeScript and Redux</p>
             <Loader />
+
+            {!authUser &&
+                <div className='need-to-login'>
+                    <p>You are not authorized, log in please:</p>
+                    <Link to={urlEndpoints.auth} className='link'>to Auth page</Link>
+                </div>
+            }
         </div>
     );
 };
